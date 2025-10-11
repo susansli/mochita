c9f0e775247ce621240dc0291a667a18308f7c23
Susan Li <susansemail195@gmail.com>
2025-10-10 19:54:11 -0700

    Add bg and mochita placeholder

--- app/pages/tutorial.tsx
+++ app/pages/tutorial.tsx
@@ -3,7 +3,7 @@ import TopStatusBar from "@/components/status/TopStatusBar";
 import { useFocusEffect } from "expo-router";
 import { useSetAtom } from "jotai";
 import { useCallback, useState } from "react";
-import { View } from "react-native";
+import { View, ImageBackground, StyleSheet, Image } from "react-native";
 import { withPageWrapper } from "../../components/wrappers/withPageWrapper";
 
 function Tutorial() {
@@ -19,7 +19,18 @@ function Tutorial() {
 
   return (
     <View className="flex-1">
+      <ImageBackground
+        source={{ uri: "https://i.imgur.com/WDcPERN.jpeg" }}
+        resizeMode="cover"
+        style={StyleSheet.absoluteFillObject}
+        imageStyle={{}}
+      />
       {!isTopStatusHidden && <TopStatusBar />}
+      <Image
+        source={{ uri: "https://i.imgur.com/n66Cu8e.gif" }}
+        className="h-[60%] w-[60%] absolute mt-[85%] ml-[40%]"
+        resizeMode="contain"
+      />
     </View>
   );
 }
