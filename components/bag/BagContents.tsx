b7dc04a373da36a20f2b2368501b5cc3c960b559
Susan Li <susansemail195@gmail.com>
2025-10-28 21:21:19 -0700

    Bag slots

--- components/bag/BagContents.tsx
+++ components/bag/BagContents.tsx
@@ -1,20 +1,27 @@
+import { equippedItemsAtom } from "@/atoms/bagAtoms";
+import { EquippedItems } from "@/data/dataInterfaces";
+import { ItemType } from "@/util/enums";
+import { useAtom } from "jotai";
 import { View } from "react-native";
 import BagSlot from "./BagSlot";
 
 export default function BagContents() {
+
+  const [equippedItems, setEquippedItems] = useAtom<EquippedItems>(equippedItemsAtom);
+
   return (
     <View className="flex-row flex-wrap p-5 rounded-lg bg-teal-500 mt-7">
       <View className="w-1/2 p-2 items-center">
-        <BagSlot />
+        <BagSlot item={equippedItems[ItemType.TICKET]} />
       </View>
       <View className="w-1/2 p-2 items-center">
-        <BagSlot />
+        <BagSlot item={equippedItems[ItemType.BAG]} />
       </View>
       <View className="w-1/2 p-2 items-center">
-        <BagSlot />
+        <BagSlot item={equippedItems[ItemType.LUCKY_CHARM]} />
       </View>
       <View className="w-1/2 p-2 items-center">
-        <BagSlot />
+        <BagSlot item={equippedItems[ItemType.SNACK]} />
       </View>
     </View>
   );
--- components/bag/BagSlot.tsx
+++ components/bag/BagSlot.tsx
@@ -1,11 +1,20 @@
-import { View } from "react-native";
+import { ItemCardData } from "@/data/dataInterfaces";
+import { Image, View } from "react-native";
 
 interface Props {
-    link?: string;
+  item?: ItemCardData;
 }
 
 export default function BagSlot(props: Props) {
-    return (
-        <View className="h-[7rem] w-[7rem] rounded-full bg-white" />
-    )
+  return (
+    <View className="h-[7rem] w-[7rem] rounded-full bg-white items-center justify-center">
+      {props?.item && (
+        <Image
+          source={{ uri: props.item.imgUrl }}
+          className="h-[5rem] w-[5rem]"
+          resizeMode="contain"
+        />
+      )}
+    </View>
+  );
 }
