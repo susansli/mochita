caf05d1fcacff70553a70d3cb9a9e53d38d308c4
Susan Li <susansemail195@gmail.com>
2025-10-31 15:36:56 -0700

    Header styling

--- app/pages/journal.tsx
+++ app/pages/journal.tsx
@@ -1,37 +1,48 @@
 import PageHeader from "@/components/nav/PageHeader";
+import { Button } from "@/components/ui/button";
+import { Text } from "@/components/ui/text";
+import Spacer from "@/components/utility/Spacer";
+import { Calendar, Pen } from "lucide-react-native";
 import { useState } from "react";
-import { Button, View } from "react-native";
-import DateTimePickerModal from "react-native-modal-datetime-picker";
+import { View } from "react-native";
 import { withPageWrapper } from "../../components/wrappers/withPageWrapper";
+import Hr from "@/components/utility/Hr";
 
 function Journal() {
-  const [isDatePickerVisible, setDatePickerVisibility] =
-    useState<boolean>(false);
+  const [startDate, setStartDate] = useState<string>(
+    new Date().toLocaleDateString()
+  );
+  const [endDate, setEndDate] = useState<string>("");
 
-  const showDatePicker = () => {
-    setDatePickerVisibility(true);
-  };
-
-  const hideDatePicker = () => {
-    setDatePickerVisibility(false);
-  };
-
-  const handleConfirm = (date: any) => {
-    console.warn("A date has been picked: ", date);
-    hideDatePicker();
-  };
+  function renderDateTitle() {
+    if (startDate.length && endDate.length) {
+      return `☀️ From ${startDate} to ${endDate}`;
+    }
+    return `☀️ ${startDate}`;
+  }
 
   return (
     <View className="flex-1 bg-stone-200 p-5">
       <PageHeader title="Journal with Mochita" />
-      <View className="flex-row">
-        <Button title="Show Date Picker" onPress={showDatePicker} />
-        <DateTimePickerModal
-          isVisible={isDatePickerVisible}
-          mode="date"
-          onConfirm={handleConfirm}
-          onCancel={hideDatePicker}
-        />
+      <View className="flex-row mt-[2rem] items-center">
+        <Text className="font-medium text-xl">{renderDateTitle()}</Text>
+      </View>
+      <Hr />
+      <View className="flex-row gap-2">
+        <Spacer />
+        <Button className="bg-teal-600 flex-row items-center gap-2 px-3 py-2">
+          <Calendar size={18} color="#fff" strokeWidth={2} />
+          <Text className="text-white">Filter Dates</Text>
+        </Button>
+
+        <Button
+          variant="outline"
+          className="border"
+          style={{ borderColor: "#0d9488" }}
+        >
+          <Pen size={18} color="#0d9488" strokeWidth={2} />
+          <Text className="text-teal-600">Create Entry</Text>
+        </Button>
       </View>
     </View>
   );
